245c501454efdbb68761175420ab702b249de9bd
Philip Gandy <pgandy@applied-insight.com>
2023-08-01 13:10:50 -0400

    Script to move files off instance store to S3

---
+++ src/main/java/com/ai/aws/scripts/EC2toS3.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Directory to watch for new files
+WATCH_DIR="/home/ec2-user/flightfeed/data/"
+
+# S3 bucket and path to upload to
+S3_BUCKET="s3://ai-hackathon-event-bucket/"
+
+# Function to handle file upload
+upload_to_s3() {
+    local file_path="$1"
+    local file_name=$(basename "$file_path")
+    echo "Processing file $file_name at $file_path"
+    
+    # Copy the file to S3
+    aws s3 cp "$file_path" "$S3_BUCKET$file_name"
+
+    # Optionally, delete the file from the instance store
+    rm "$file_path"
+}
+
+# Handle existing files in the directory
+for existing_file in "$WATCH_DIR"*; do
+    if [ -f "$existing_file" ]; then
+        upload_to_s3 "$existing_file"
+    fi
+done
+
+# Loop forever, watching for files to be created in the watch directory
+inotifywait -m "$WATCH_DIR" -e create |
+    while read path action file; do
+        upload_to_s3 "$path/$file"
+    done
