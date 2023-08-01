f8667330bae26c535d6232fd027dece16c084486
Ron Wilcom <rwilcom@applied-insight.com>
2023-08-01 12:56:28 -0400

    *

--- src/main/java/com/ai/aws/adapters/lambda/OpenSkyAdapterLambda.java
+++ src/main/java/com/ai/aws/adapters/lambda/OpenSkyAdapterLambda.java
@@ -8,10 +8,6 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 public class OpenSkyAdapterLambda implements RequestHandler<String, Integer>{
 
   @Override
-  /*
-   * Takes in an InputRecord, which contains two integers and a String.
-   * Logs the String, then returns the sum of the two Integers.
-   */
   public Integer handleRequest(String rawFile, Context context){
     LambdaLogger logger = context.getLogger();
     logger.log("RawFile found: " + rawFile);
